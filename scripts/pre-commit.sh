097d244204abbd04170d3c96a143cf6c6903484d
Lukas Bachlechner <35543080+lukasbachlechner@users.noreply.github.com>
2022-09-30 19:04:44 +0200

    edit pre-commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ -z $(git status -s -- '**/*.dart') ]]
+then
+    printf "\e[33;1m%s\e[0m\n" 'No changes to Dart files - go ahead'
+    exit 0
+else 
+    printf "\e[33;1m%s\e[0m\n" 'Has changes to Dart files'
+fi
+
 printf "\e[33;1m%s\e[0m\n" 'Pre-Commit'
 
 # Undo the stash of the files
@@ -9,6 +17,16 @@ pop_stash_files () {
         git stash pop
     fi
 }
+
+# Flutter Analyzer
+printf "\e[33;1m%s\e[0m\n" '=== Running Flutter analyzer ==='
+flutter analyze
+if [ $? -ne 0 ]; then
+  printf "\e[31;1m%s\e[0m\n" '=== Flutter analyzer error ==='
+  exit 1
+fi
+printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter analyzer'
+printf '%s\n' "${avar}"
 
 # Stash unstaged files
 hasChanges=$(git diff)
@@ -27,17 +45,6 @@ if [ -n "$hasNewFilesFormatted" ]; then
     printf "\e[33;1m%s\e[0m\n" 'Formmated files added to git stage'
 fi
 printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter Formatter'
-printf '%s\n' "${avar}"
-
-# Flutter Analyzer
-printf "\e[33;1m%s\e[0m\n" '=== Running Flutter analyzer ==='
-flutter analyze
-if [ $? -ne 0 ]; then
-  printf "\e[31;1m%s\e[0m\n" '=== Flutter analyzer error ==='
-  pop_stash_files
-  exit 1
-fi
-printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter analyzer'
 printf '%s\n' "${avar}"
 
 # Unit tests

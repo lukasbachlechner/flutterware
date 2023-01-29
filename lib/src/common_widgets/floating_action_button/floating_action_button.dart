7e56f4b7a6a0b659fc077aba87b8caa190ada856
Lukas Bachlechner <35543080+lukasbachlechner@users.noreply.github.com>
2023-01-29 19:18:14 +0100

    fix some weird file issues

--- lib/src/common_widgets/floating_action_button/floating_action_button.dart
+++ lib/src/common_widgets/floating_action_button/floating_action_button.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutterware/src/common_widgets/floating_action_button/floating_action_button_badge.dart';
 import 'package:flutterware/src/constants/app_sizes.dart';
+import 'package:flutterware/src/features/cart/data/cart_button_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../constants/flutterware_icons.dart';
 import '../circle_button/circle_button.dart';
 
-class FwFloatingActionButton extends StatelessWidget {
+class FwFloatingActionButton extends ConsumerWidget {
   const FwFloatingActionButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cartButtonNotifier = ref.watch(cartButtonNotifierProvider);
+
     return SizedBox.square(
       dimension: AppSizes.fabSize,
       child: Stack(
         children: [
           CircleButton(
-            iconData: FlutterwareIcons.basket,
+            iconData: cartButtonNotifier.iconData,
             size: AppSizes.fabSize,
-            onPressed: () {},
+            onPressed: cartButtonNotifier.onPressed,
+            color: cartButtonNotifier.buttonBackgroundColor,
           ),
           const Align(
             alignment: Alignment.topRight,
--- lib/src/common_widgets/floating_action_button/floating_action_button_badge.dart
+++ lib/src/common_widgets/floating_action_button/floating_action_button_badge.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutterware/src/constants/app_colors.dart';
+import 'package:flutterware/src/features/cart/data/cart_button_provider.dart';
+import 'package:flutterware/src/features/cart/data/cart_repository.dart';
 import 'package:flutterware/src/utils/string.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../constants/app_sizes.dart';
 
-class FwFloatingActionButtonBadge extends StatelessWidget {
+class FwFloatingActionButtonBadge extends ConsumerWidget {
   const FwFloatingActionButtonBadge({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cartItemCount = ref.watch(cartNotifierProvider.notifier).productCount;
+    final cartButtonState = ref.watch(cartButtonNotifierProvider);
+
+    if (cartItemCount == 0 || cartButtonState is! InitialCartButtonState) {
+      return const SizedBox.shrink();
+    }
+
     return Container(
       padding: const EdgeInsets.all(2),
       decoration: const ShapeDecoration(
@@ -24,7 +34,7 @@ class FwFloatingActionButtonBadge extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-            limitToThreshold(1),
+            limitToThreshold(cartItemCount),
             textAlign: TextAlign.center,
             // TODO: refactor styling
             style: const TextStyle(

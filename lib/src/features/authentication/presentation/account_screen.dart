2f90674352e1dcc84164432eba5da9d7fd4b2d3c
Lukas Bachlechner <35543080+lukasbachlechner@users.noreply.github.com>
2023-01-29 19:13:55 +0100

    feat: auth (wip)

---
+++ lib/src/features/authentication/presentation/account_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  static const path = '/account';
+  const AccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Account'));
+  }
+}

9e1a1a4d46936136318ef18b84775a39cfba3080
Lukas Bachlechner <35543080+lukasbachlechner@users.noreply.github.com>
2023-01-29 19:14:12 +0100

    feat: wishlist (wip)

---
+++ lib/src/features/wishlist/presentation/wishlist_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  static const path = '/wishlist';
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Wishlist'));
+  }
+}

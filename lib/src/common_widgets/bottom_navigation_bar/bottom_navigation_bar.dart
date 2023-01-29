229a763698847a48fccbd87c6a88c84d55831ee9
Lukas Bachlechner <35543080+lukasbachlechner@users.noreply.github.com>
2023-01-29 19:18:14 +0100

    fix some weird file issues

--- lib/src/common_widgets/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/src/common_widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutterware/src/constants/app_colors.dart';
+import 'package:flutterware/src/constants/app_sizes.dart';
 import 'package:flutterware/src/constants/flutterware_icons.dart';
+import 'package:flutterware/src/features/authentication/presentation/account_screen.dart';
+import 'package:flutterware/src/features/cart/data/cart_button_provider.dart';
+import 'package:flutterware/src/features/home/presentation/home_screen.dart';
+import 'package:flutterware/src/features/menu/presentation/screens/menu_screen.dart';
+import 'package:flutterware/src/features/wishlist/presentation/wishlist_screen.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class FwBottomNavigationBar extends StatelessWidget {
+class FwBottomNavigationBar extends ConsumerWidget {
   const FwBottomNavigationBar({
     super.key,
   });
-
-  Widget _buildItem({required Widget icon, required String label}) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        icon,
-        Text(label),
-      ],
-    );
-  }
 
   Widget _buildFabPlaceholder({required String label}) {
     return Column(
@@ -23,47 +21,101 @@ class FwBottomNavigationBar extends StatelessWidget {
       children: [
         const SizedBox(
           height: 24,
-          width: 66,
+          width: AppSizes.fabSize,
         ),
-        Text(label),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          label,
+          style: const TextStyle(fontSize: 12.0, fontWeight: FontWeight.w300),
+        ),
       ],
     );
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final safeBottomInset = MediaQuery.of(context).padding.bottom;
     return SizedBox(
-      height: 60,
+      height: 60 + safeBottomInset,
       child: BottomAppBar(
         color: AppColors.white,
         child: Padding(
-          padding: const EdgeInsets.only(left: 30.0, right: 16),
+          padding: const EdgeInsets.only(left: 30.0, right: 16.0, top: 10.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             mainAxisSize: MainAxisSize.max,
             children: [
-              _buildItem(
-                icon: const Icon(
-                  FlutterwareIcons.home,
-                ),
+              const FwBottomNavigationBarItem(
+                icon: FlutterwareIcons.home,
                 label: 'Home',
+                path: HomeScreen.path,
               ),
-              _buildItem(
-                icon: const Icon(FlutterwareIcons.menu),
+              const FwBottomNavigationBarItem(
+                icon: FlutterwareIcons.menu,
                 label: 'Menu',
+                path: MenuScreen.path,
               ),
-              _buildItem(
-                icon: const Icon(FlutterwareIcons.favorites),
+              const FwBottomNavigationBarItem(
+                icon: FlutterwareIcons.favorites,
                 label: 'Wishlist',
+                path: WishlistScreen.path,
               ),
-              _buildItem(
-                icon: const Icon(FlutterwareIcons.account),
+              const FwBottomNavigationBarItem(
+                icon: FlutterwareIcons.account,
                 label: 'Account',
+                path: AccountScreen.path,
               ),
-              _buildFabPlaceholder(label: 'Cart'),
+              _buildFabPlaceholder(
+                label: ref.watch(cartButtonNotifierProvider).text,
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FwBottomNavigationBarItem extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final String path;
+
+  const FwBottomNavigationBarItem({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.path,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isActive = GoRouter.of(context).location.startsWith(path);
+
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () => context.go(path),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            color: isActive ? AppColors.primaryColor : null,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            label,
+            style: TextStyle(
+              color: isActive ? AppColors.primaryColor : null,
+              fontWeight: FontWeight.w300,
+              fontSize: 12.0,
+            ),
+          ),
+        ],
       ),
     );
   }
